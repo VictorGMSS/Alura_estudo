d99243ad802f5f6e4ac6164a1f0ce4af64d360d4
VictorGMSS <Souzavictor51@yahoo.com.br>
2021-12-13 15:26:40 -0300

    tentativa 3

---
+++ receitas/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404, get_list_or_404, render
+from .models import Receita
+#from django.http import HttpResponse
+
+def index(request):
+    #return HttpResponse('<h1>Receitas</h1> <h2>Bem-vindo</h2>')
+    receitas = Receita.objects.filter(publicada=True)
+
+    dados = {
+        'receitas' : receitas
+    }
+
+    return render(request, 'index.html', dados)
+
+
+def receita(request, receita_id):
+    #return HttpResponse('<h1>Receitas</h1> <h2>Bem-vindo</h2>')
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
