651caeee47779085c622e4817ed32de2ee437086
VictorGMSS <victorgabriel@unisantos.br>
2021-12-15 11:12:28 -0300

    implementação de buscas

--- alura/settings.py
+++ alura/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'AluraDB',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'Vi061199@@@',
         'HOST': 'localhost'
     }
 }
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alura/static')
 
 ]
+
+#media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
